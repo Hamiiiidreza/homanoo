278dc2512134728253e5c82f20738dc6d2f26ed5
Hamiiiidreza <ha79mi45dd@gmail.com>
2026-07-23 20:07:17 +0330

    fix: update category types

--- src/types/category.types.ts
+++ src/types/category.types.ts
@@ -1,7 +1,12 @@
-export type CategoryCardProps = {
-    image: string;
-    title: string; 
+export type CategoriesProps = {
+    data: {
+        description: string;
+        image: string;
+        isActive: boolean;
+        name: string;
+        slug: string;
+    };
     isTop: boolean;
     imageClass?: string;
     hasInnerLeftBorder?: boolean;
-}
+};
