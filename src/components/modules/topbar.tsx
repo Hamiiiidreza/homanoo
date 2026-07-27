373d1406a1e778d68b09f040687b062268d81d04
Hamiiiidreza <ha79mi45dd@gmail.com>
2026-07-27 11:30:04 +0330

    feat(topbar): add shop page link

--- src/components/modules/topbar.tsx
+++ src/components/modules/topbar.tsx
@@ -1,13 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { IoCloseOutline } from "react-icons/io5";
-import ArrowLink from "../ui/arrow-link";
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 type TopbarProps = {
   onClose?: () => void;
   forceHide?: boolean;
 };
 
-const Topbar: React.FC<TopbarProps> = ({ onClose, forceHide = false }) => {
+const Topbar = ({ onClose, forceHide = false }: TopbarProps) => {
   const [showBanner, setShowBanner] = useState<boolean>(true);
   const [visible, setVisible] = useState<boolean>(true);
 
@@ -15,39 +14,30 @@ const Topbar: React.FC<TopbarProps> = ({ onClose, forceHide = false }) => {
     if (forceHide) setVisible(false);
   }, [forceHide]);
 
-  const handleClose = () => {
-    setVisible(false);
-    if (onClose) onClose();
-  };
-
   if (!visible) return null;
 
   return (
     <>
       {showBanner && (
-        <div className="relative flex items-center justify-center gap-4 w-full h-10 bg-main px-8 sm:px-0">
-          <div className="inline-flex items-center justify-center gap-3 relative">
+        <Link
+          to={'/shop'}
+          className="bg-main relative flex h-10 w-full items-center justify-center gap-4 px-3 sm:px-0"
+        >
+          <div className="relative inline-flex items-center justify-center gap-3">
             <img
-              style={{ filter: "invert(1)" }}
+              style={{ filter: 'invert(1)' }}
               src="/Images/ticket-percent.svg"
-              className="relative size-6 hover:drop-shadow-custom transition-all"
+              className="relative size-6 transition-all"
             />
-            <p className="font-VazirBold text-white leading-[22px] text-sm text-center">
+            <p className="text-center text-sm leading-[22px] font-bold text-white">
               ۳۰٪ تخفیف روی تمام محصولات — مدت محدود!
             </p>
           </div>
-          {/* <div className="hidden sm:inline-flex">
-            <ArrowLink
-              title="مشاهده فروشگاه"
-              textColor="text-secondary-color-blue"
-              borderColor="border-secondary-color-blue"
-              to="/Shop"
-            />
-          </div> */}
-        </div>
+        </Link>
       )}
     </>
   );
 };
 
 export default Topbar;
+
