973d7d4b0f3cc031d0e2c7c1295098fdc18031b8
Hamiiiidreza <ha79mi45dd@gmail.com>
2026-08-01 19:17:09 +0330

    refactor: redesign cart sidebar

--- src/components/modules/cart-sidebar.tsx
+++ src/components/modules/cart-sidebar.tsx
@@ -1,98 +1,83 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { Button } from "../ui/button";
-import { CartItems } from "../../types/cart.types";
-import { User } from "../../types/user.types";
-
-import {
-    Sheet,
-    SheetContent,
-} from "../ui/sheet";
+import { Link } from 'react-router-dom';
+import { Button } from '../ui/button';
+import MobileProductCard from '../screens/cart/partials/mobile-product-card';
+import { CartItem, User } from '../../types/user.types';
+import { X } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { getCartTotalPrice } from '../../utils/helpers';
 
 type CartSidebarProps = {
-    data: User | undefined;
+    data: User;
     open: boolean;
     onClose: () => void;
-}
-
-const cartItems: CartItems[] = [
-    { id: 1, name: "میز سینی", color: "مشکی", price: 19.19, quantity: 2, image: "/Images/product-5.png" },
-    { id: 2, name: "میز سینی", color: "قرمز", price: 19.19, quantity: 2, image: "/Images/product-5-2.svg" },
-    { id: 3, name: "چراغ رومیزی", color: "طلایی", price: 39.0, quantity: 2, image: "/Images/product-2.png" },
-];
+};
 
 export default function CartSidebar({ data, open, onClose }: CartSidebarProps) {
-    const navigate = useNavigate();
-    const subtotal = 99.0;
-    const total = 234.0;
+    const [totalPrice, setTotalPrice] = useState<number | null>(null);
 
-    const handleCheckout = (): void => {
-        navigate('/checkout')
-    }
+    useEffect(() => {
+        if (data?.cart) {
+            setTotalPrice(getCartTotalPrice(data.cart));
+        }
+    }, [data]);
 
     return (
-        <Sheet open={open} onOpenChange={(v) => !v && onClose()}>
-            <SheetContent
-                side="right"
-                className="flex flex-col w-[400px] h-screen bg-white px-6 py-10 shadow-lg"
+        <div className={`fixed inset-0 z-40 ${open ? 'visible' : 'invisible'}`}>
+            <div
+                onClick={onClose}
+                className={`absolute inset-0 bg-black/40 transition-opacity duration-300 ${open ? 'opacity-100' : 'opacity-0'
+                    }`}
+            />
+            <aside
+                className={`fixed top-0 right-0 z-50 flex h-screen w-[400px] transform flex-col rounded-l-2xl bg-white px-6 py-6 shadow-lg transition-transform duration-300 ease-in-out ${open ? 'translate-x-0' : 'translate-x-full'} `}
             >
-                <header className="flex items-center justify-between w-full mb-10">
-                    <h1 className="font-VazirMedium text-[28px] text-black/900 transition-all hover:drop-shadow-custom">سبد خرید</h1>
-                </header>
+                <div className="mb-10 flex w-full items-center justify-between">
+                    <h1 className="tracking-headline-6 text-[28px] leading-[34px] text-black/900 transition-all">
+                        سبد خرید شما
+                    </h1>
+                    <X
+                        className="stroke-neutral-04 cursor-pointer transition-all"
+                        onClick={onClose}
+                    />
+                </div>
 
-                <div className="flex-1 overflow-y-auto space-y-4 pr-2 max-h-[350px] scrollbar-custom">
-                    {cartItems.map((item: CartItems) => (
-                        <div key={item.id} className="flex items-center border-b border-gray-200 pb-6">
-                            <div className="bg-neutral-02">
-                                <img src={item.image} alt={item.name} className="w-20 h-24 object-cover bg-blend-multiply transition-all hover:shadow-[0_4px_4px_rgba(0,0,0,0.25)]" />
-                            </div>
-                            <div className="flex justify-between flex-1 mx-4">
-                                <div className="space-y-2">
-                                    <h2 className="font-VazirBold text-sm text-neutral-07 transition-all hover:drop-shadow-custom">{item.name}</h2>
-                                    <p className="font-VazirRegular text-neutral-04 text-xs transition-all hover:drop-shadow-custom">رنگ: {item.color}</p>
-                                    <div className="flex items-center border border-neutral-04 rounded mt-2 w-20 h-8 justify-between px-2 transition-all hover:shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
-                                        <button>-</button>
-                                        <span className="text-xs font-VazirBold">{item.quantity}</span>
-                                        <button>+</button>
-                                    </div>
-                                </div>
-                                <div className="flex flex-col items-end gap-2">
-                                    <span className="font-VazirBold text-sm transition-all hover:drop-shadow-custom">${item.price.toFixed(2)}</span>
-                                </div>
-                            </div>
-                        </div>
+                <div className="scrollbar-custom flex-1 space-y-6 overflow-y-auto bg-white">
+                    {data?.cart.map((item: CartItem) => (
+                        <MobileProductCard
+                            total={data?.cart.length}
+                            onClose={onClose}
+                            item={item}
+                        />
                     ))}
                 </div>
 
-                <div className="mt-6">
-                    <div className="flex justify-between border-b border-gray-200 py-3">
-                        <span className="font-VazirRegular text-base">جمع جزئی</span>
-                        <span className="font-VazirRegular text-base">${subtotal.toFixed(2)}</span>
-                    </div>
-                    <div className="flex justify-between py-3">
-                        <span className="font-VazirMedium text-xl">مجموع</span>
-                        <span className="font-VazirMedium text-xl">${total.toFixed(2)}</span>
-                    </div>
+                <div className="flex justify-between py-3">
+                    <span className="text-neutral-07 text-xl leading-[28px] transition-all">
+                        مجموع
+                    </span>
+                    <span className="text-neutral-07 text-xl leading-[28px] transition-all">
+                        {totalPrice?.toLocaleString()} هزار تومان
+                    </span>
                 </div>
 
-                <div className="flex flex-col gap-4 mt-4">
-                    <Button
-                        onClick={handleCheckout}
-                        className="font-VazirMedium w-full h-[52px] bg-main text-white hover:bg-main/90 cursor-pointer"
-                    >
-                        تسویه حساب
-                    </Button>
-
+                {/* Buttons */}
+                <div className="mt-4 flex flex-col gap-4">
+                    <Link to="/checkout" className="w-full">
+                        <Button className="bg-main hover:!bg-main h-12 w-full cursor-pointer transition-opacity hover:opacity-70">
+                            تکمیل سفارش
+                        </Button>
+                    </Link>
                     <Link
-                        to="/shopping-cart"
-                        className="font-VazirBold text-sm text-black/900 mx-auto border-b border-black/900 pb-1"
+                        to="/cart"
+                        className="mx-auto cursor-pointer border-b border-black/900 pb-1 text-sm leading-[22px] text-black/900 transition-all"
                     >
                         مشاهده سبد خرید
                     </Link>
                 </div>
-            </SheetContent>
-        </Sheet>
+            </aside>
+        </div>
     );
 }
+
 
 
