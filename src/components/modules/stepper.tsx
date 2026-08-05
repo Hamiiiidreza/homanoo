938afd102765112fc3677f5dd60ef30612d1d816
Hamiiiidreza <ha79mi45dd@gmail.com>
2026-08-05 21:42:25 +0330

    refactor: improve stepper component

--- src/components/modules/stepper.tsx
+++ src/components/modules/stepper.tsx
@@ -1,29 +1,27 @@
-import React from "react";
-
 type StepperProps = {
     title: string;
     currentStep: number;
-}
+};
 
 type Step = {
     id: number;
     name: string;
 };
 
-const Stepper: React.FC<StepperProps> = ({ title, currentStep }) => {
+const Stepper = ({ title, currentStep }: StepperProps) => {
     const steps: Step[] = [
-        { id: 1, name: "سبد خرید" },
-        { id: 2, name: "اطلاعات پرداخت " },
-        { id: 3, name: "تکمیل سفارش" },
+        { id: 1, name: 'سبد خرید' },
+        { id: 2, name: 'اطلاعات پرداخت' },
+        { id: 3, name: 'تکمیل سفارش' },
     ];
 
     return (
-        <div className="w-full flex flex-col items-center gap-8">
-            <h2 className="font-VazirMedium text-neutral-07 text-3xl sm:text-4xl lg:text-[54px] text-center transition-all hover:drop-shadow-custom">
+        <div className="flex w-full flex-col items-center gap-8">
+            <h2 className="text-neutral-07 text-center text-3xl transition-all sm:text-4xl lg:text-[54px]">
                 {title}
             </h2>
 
-            <div className="w-[80%] flex justify-center">
+            <div className="flex w-full justify-center">
                 <div className="flex w-full max-w-5xl gap-4 sm:gap-6 lg:gap-8">
                     {steps.map((step) => {
                         const isCompleted = currentStep > step.id;
@@ -36,58 +34,26 @@ const Stepper: React.FC<StepperProps> = ({ title, currentStep }) => {
                         return (
                             <div
                                 key={step.id}
-                                className={`
-                  flex flex-col pb-4
-                  sm:flex-1
-
-                  ${!isVisibleOnMobile
-                                        ? "hidden sm:flex"
-                                        : "flex"
-                                    }
-
-                  ${isCurrent
-                                        ? "flex-1"
-                                        : ""
-                                    }
-
-                  ${isCompleted
-                                        ? "border-b-2 border-secondary-color-green"
-                                        : ""
-                                    }
-
-                  ${isCurrent
-                                        ? "border-b-2 border-[#23262F]"
-                                        : ""
-                                    }
-                `}
+                                className={`flex flex-col pb-4 sm:flex-1 ${!isVisibleOnMobile ? 'hidden sm:flex' : 'flex'} ${isCurrent ? 'flex-1' : ''} ${isCompleted || currentStep == 3
+                                        ? 'border-secondary-color-green border-b-2'
+                                        : ''
+                                    } ${isCurrent ? 'border-b-2 border-[#23262F]' : ''} `}
                             >
-                                <div className="flex items-center gap-3">
+                                <div className="flex items-center justify-center gap-3">
                                     <div
-                                        className={`
-                      flex items-center justify-center rounded-full size-10 shrink-0
-                      ${isCompleted ? "bg-secondary-color-green text-white" : ""}
-                      ${isCurrent ? "bg-main text-white" : ""}
-                      ${isUpcoming ? "bg-[#B1B5C3] text-white" : ""}
-                    `}
+                                        className={`flex size-10 shrink-0 items-center justify-center rounded-full ${isCompleted || currentStep == 3 ? 'bg-secondary-color-green text-white' : ''} ${isCurrent ? 'bg-main text-white' : ''} ${isUpcoming ? 'bg-[#B1B5C3] text-white' : ''} `}
                                     >
-                                        {isCompleted ? (
+                                        {isCompleted || currentStep == 3 ? (
                                             <img src="/Images/tick.svg" className="size-6" />
                                         ) : (
-                                            <span className="font-VazirBold text-base">
+                                            <span className="text-base font-bold">
                                                 {step.id.toLocaleString('fa-IR')}
                                             </span>
                                         )}
                                     </div>
 
                                     <div
-                                        className={`
-                      font-VazirBold text-sm sm:text-base truncate
-                      ${step.id !== currentStep ? "hidden sm:block" : "block"}
-
-                      ${isCompleted ? "text-secondary-color-green" : ""}
-                      ${isCurrent ? "text-[#23262F]" : ""}
-                      ${isUpcoming ? "text-[#B1B5C3]" : ""}
-                    `}
+                                        className={`truncate text-center text-sm font-bold sm:text-base ${step.id !== currentStep ? 'hidden sm:block' : 'block'} ${isCompleted ? 'text-secondary-color-green' : ''} ${isCurrent && step.id !== 3 ? 'text-[#23262F]' : 'text-[#23262F]'} ${isUpcoming ? 'text-[#B1B5C3]' : ''} `}
                                     >
                                         {step.name}
                                     </div>
@@ -99,7 +65,7 @@ const Stepper: React.FC<StepperProps> = ({ title, currentStep }) => {
             </div>
         </div>
     );
-}
+};
 
 export default Stepper;
 
