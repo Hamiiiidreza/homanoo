75edbc3b9e055985dafb278f38c4f672dbb8de4f
Hamiiiidreza <ha79mi45dd@gmail.com>
2026-08-15 21:37:20 +0330

    fix(contact-us): adjust position coordinates

--- src/components/screens/contact-us/contact-us-screen.tsx
+++ src/components/screens/contact-us/contact-us-screen.tsx
@@ -12,7 +12,7 @@ const ContactUsScreen = () => {
                 <div className="lg:before:!rounded-b-l-none before:content-[] before:bg-main relative flex w-full flex-col-reverse gap-8 rounded-3xl bg-white p-6 shadow-[0px_4px_24px_0px_#00000014] before:absolute before:right-0 before:bottom-0 before:h-[160px] before:w-full before:rounded-b-3xl lg:!flex-row lg:before:!top-0 lg:before:!h-full lg:before:!w-[20%] lg:before:!rounded-r-3xl">
                     <div className="flex w-full lg:!p-2">
                         <Map
-                            position={[29.87739213881141, 52.806453926799264]}
+                            position={[35.7237783, 51.4042813]}
                             className="!h-[200px] !w-full sm:!h-[300px] lg:!h-auto lg:!w-[83%]"
                         />
                     </div>
