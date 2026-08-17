55df4b61eaa250af23204372d73867feab7c4201
Hamiiiidreza <ha79mi45dd@gmail.com>
2026-08-17 11:29:58 +0330

    refactor

--- src/components/screens/home/partials/hero.tsx
+++ src/components/screens/home/partials/hero.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Hero: React.FC = () => (
   <section className="flex flex-col lg:flex-row items-center md:!pt-13 pt-10 justify-between gap-4 md:gap-8 w-full bg-white">
-    <h2 className="w-[57.5] text-xl md:text-3xl xl:text-4xl font-VazirMedium text-center tracking-hero hover:drop-shadow-custom transition-all">
+    <h2 className="w-[57.5%] text-xl md:text-3xl xl:text-4xl font-VazirMedium text-center tracking-hero hover:drop-shadow-custom transition-all">
       <span className="text-neutral-07 inline-block">بهترین قیمت و تنوع لوازم خانگی در هومانو</span>
     </h2>
 
-    <div className="md:!w-[42.5] w-full lg:max-w-md">
+    <div className="md:!w-[42.5%] w-full lg:max-w-md">
       <p className="font-VazirRegular text-base leading-1.625 text-neutral-04 hover:drop-shadow-custom text-justify transition-all">
         <span className="text-base font-VazirBold leading-1.625 text-main">
           هومانو
